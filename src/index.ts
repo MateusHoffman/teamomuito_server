6a24cf0b8d1aeaaf6102528d231bffeaea3b24af
Hoffman <mateushoffmandev@gmail.com>
2024-09-29 13:57:35 -0300

    index atualizado

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middleware de CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "http://localhost:3000", // Adicionando um valor padrão para desenvolvimento
     methods: ["GET", "POST"],
     credentials: true,
   })
@@ -22,8 +22,10 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+// Rotas da API
 app.use("/api", routes);
 
+// Conectar ao MongoDB
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => console.log("MongoDB connected"))
@@ -35,6 +37,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send("Something broke!");
 });
 
+// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
