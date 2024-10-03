411661a542542e732b97a8993335c4f35366cf61
Hoffman <mateushoffmandev@gmail.com>
2024-10-03 17:25:39 -0300

    200mb

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
 // Conectar ao banco de dados
 connectDatabase();
